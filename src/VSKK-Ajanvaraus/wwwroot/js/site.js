93393cd1a18b1d89cc95cfd60ff43596aa551ead
Pekka Lukkari <pekkaj.lukkari@gmail.com>
2016-09-09 14:46:50 +0300

    Lisätty demoon poiston varmistuspainike.

--- src/VSKK-Ajanvaraus/wwwroot/js/site.js
+++ src/VSKK-Ajanvaraus/wwwroot/js/site.js
@@ -10,22 +10,15 @@ $(document).ready(function () {
     var containerPotilaanVarauksetTable = ".container-potilaan-varaukset div.table-responsive";
     var containerPotilaanVarauksetEi = ".container-potilaan-varaukset .container-ei-loytynyt";
 
-    $(".container-potilaan-varaukset table tbody tr").on('click', function () {
+    var kaikkiPotilaanVaraukset = "container-potilaan-varaukset table tbody tr";
 
-        var valittuRivi = $(this);
-        var varattuAika = valittuRivi.find("#txtVarattuAika").text();
-        var toimipiste = valittuRivi.find("#txtToimipiste").text();
-        var palvelu = valittuRivi.find("#txtPalvelu").text();
-        var tilaavaYksikko = valittuRivi.find("#txtTilaavaYksikko").text();
-        var lisatiedot = valittuRivi.find("#txtLisatiedot").text();
+    var btnPeruAika = "#btnPeruAika";
+    var divModalBodyPeruAika = "#divModalBodyPeruAika";
+    var btnPeruAjavarausPeruutus = "#btnPeruAjavarausPeruutus";
+    var btnPeruAjanvarausVahvistus = "#btnPeruAjanvarausVahvistus";
 
-        $("#lblModalVarattuAikaArvo").text(varattuAika);
-        $("#lblModalToimipisteArvo").text(toimipiste);
-        $("#lblModalPalveluArvo").text(palvelu);
-        $("#lblModalTilaavaYksikkoArvo").text(tilaavaYksikko);
-        $("#lblModalLisatiedotArvo").text(lisatiedot);
-        $('#myModal').modal('show');
-    });
+    // Lisätään testailuja varten oikea henkilötunnus suoraan koodiin sivun latauksessa:
+    $(txtHenkilotunnus).val("010101-101X");
 
     $(btnNaytaVaraukset).on('click', function () {
         var valittunHenkilotunnus = $(txtHenkilotunnus).val();
@@ -42,8 +35,38 @@ $(document).ready(function () {
         
     });
 
-    $(btnVaraaUusiAika).on('click', function () {
+    $(".container-potilaan-varaukset table tbody tr").on('click', function () {
 
+        $(divModalBodyPeruAika).hide();
+
+        var valittuRivi = $(this);
+        var varauksenNumero = valittuRivi.data("varausnumero");
+        var varattuAika = valittuRivi.find("#txtVarattuAika").text();
+        var toimipiste = valittuRivi.find("#txtToimipiste").text();
+        var palvelu = valittuRivi.find("#txtPalvelu").text();
+        var tilaavaYksikko = valittuRivi.find("#txtTilaavaYksikko").text();
+        var lisatiedot = valittuRivi.find("#txtLisatiedot").text();
+
+        $("#varauksenNumero").val(varauksenNumero);
+        $("#lblModalVarattuAikaArvo").text(varattuAika);
+        $("#lblModalToimipisteArvo").text(toimipiste);
+        $("#lblModalPalveluArvo").text(palvelu);
+        $("#lblModalTilaavaYksikkoArvo").text(tilaavaYksikko);
+        $("#lblModalLisatiedotArvo").text(lisatiedot);
+        $('#myModal').modal('show');
+    });
+
+    $(btnPeruAika).on('click', function () {
+        $(divModalBodyPeruAika).show("slow", function () {});
+    });
+    $(btnPeruAjavarausPeruutus).on('click', function () {
+        $(divModalBodyPeruAika).hide("slow", function () { });
+    });
+    $(btnPeruAjanvarausVahvistus).on('click', function () {
+        var varauksenNumero = $('#varauksenNumero').val();
+        var merkkijono = "*[data-varausnumero=\"" + varauksenNumero + "\"]";
+        $(merkkijono).remove();
+        $('#myModal').modal('hide');
     });
 
 });
